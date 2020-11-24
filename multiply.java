c925b7f61a599597eac5536d7625fc268f49b903
dhruvsethi708 <dhruvsethi708@gmail.com>
2020-11-24 17:58:00 +0530

    change multiply

---
+++ multiply.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class multiply {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("ENTER 2 NUMBERS");    
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println(a*b);
+    }
+}
