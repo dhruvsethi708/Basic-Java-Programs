9448fcb3acdcb4add4e721c791991e5a41680a53
dhruvsethi708 <dhruvsethi708@gmail.com>
2020-11-25 14:04:38 +0530

    sixth program

---
+++ GreaterTwo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class GreaterTwo {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>b)
+        System.out.println(a + " is greater");
+        if(a<b)
+        System.out.println(b + " is greater");
+    }
+}
