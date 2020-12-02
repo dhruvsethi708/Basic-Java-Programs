2c94857a9f4e0864ff4d8fb741c3c4163a948b47
dhruvsethi708 <dhruvsethi708@gmail.com>
2020-12-02 17:34:02 +0530

    1 to 20

---
+++ Tables.java
@@ -0,0 +1,11 @@
+public class Tables {
+    public static void main(String[] args) {
+      for(int i = 1; i <= 20; i++) {
+          int temp = i;
+          for(int j = 1; j <=10; j++) {
+           System.out.print(temp*j + " ");
+          }
+          System.out.println();
+      }  
+    }
+}
